f31b1b11c42c6b27714dd2559be0a2a1896d2d5c
Jack Finnis <finnis.j@etoncollege.org.uk>
2021-08-02 16:46:20 +0100

    Initial Commit

---
+++ Petitions/PetitionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetitionsApp.swift
+//  Petitions
+//
+//  Created by William Finnis on 02/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct PetitionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
