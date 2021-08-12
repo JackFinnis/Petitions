3d79d4e831b3e66dff17be6ed7daff0ecff679f7
Jack Finnis <finnis.j@etoncollege.org.uk>
2021-08-12 13:41:51 +0100

    Initial commit

--- Petitions/ContentView.swift
+++ Petitions/ContentView.swift
@@ -7,15 +7,38 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+struct Petition: Codable, Identifiable {
+    let id: String
+    let title: String
+    let body: String
+    let signatureCount: Int
+    let signatureThreshold: Int
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct ContentView: View {
+    @State var petitions = [Petition]()
+    
+    var body: some View {
+        NavigationView {
+            List(petitions) { petition in
+                NavigationLink(destination: Text(petition.body)) {
+                    VStack {
+                        Text(petition.title)
+                            .badge("\(petition.signatureCount)/\(petition.signatureThreshold)")
+                        ProgressView(value: Double(petition.signatureCount), total: Double(petition.signatureThreshold))
+                    }
+                }
+            }
+            .navigationTitle("Petitions")
+            .task {
+                do {
+                    let url = URL(string: "https://hws.dev/petitions.json")!
+                    let (data, _) = try await URLSession.shared.data(from: url)
+                    petitions = try JSONDecoder().decode([Petition].self, from: data)
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+        }
     }
 }
